fc810dd2f047a96b679cf888b1431f5910e598ff
mahlet <akotet2123@gmail.com>
2023-04-06 03:00:48 -0700

    bug --fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
-if ![ -x "$(command nginx -v)" ];
+if ! [ -x "$(command nginx -v)" ];
 then
         sudo apt-get update
         sudo apt-get install nginx -y
 fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-test_web = "<html><head></head><body>Holberton School</body></html>" 
+test_web="<html><head></head><body>Holberton School</body></html>" 
 echo "$test_web" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/

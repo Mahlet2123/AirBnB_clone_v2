b14de050757376622b3fef5d4083a40f17bbc49f
mahlet <akotet2123@gmail.com>
2023-04-06 13:52:44 +0000

    to deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy """
+
+
+
+def do_deploy(archive_path):
+	""" distributes an archive to your web servers """
+	

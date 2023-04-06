b554d722e31e9e2971f45eaff9ae45fbb7fc5e33
mahlet <akotet2123@gmail.com>
2023-04-06 15:19:58 +0000

    ssh files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,8 +2,9 @@
 """ Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers,
 using the function do_deploy """
-from fabric.operations import put
 from os.path import exists
+from fabric.api import *
+
 
 env.hosts = ['54.90.3.149', '75.101.217.125']
 env.user = 'ubuntu'
@@ -13,10 +14,13 @@ def do_deploy(archive_path):
     if not exists(archive_path):
         return False
 
-    # Upload the archive to the /tmp/ directory of the web server
+    # Upload the archive to the
+    # /tmp/ directory of the web server
+    # put(local_path, remote_path)
     put(archive_path, "/tmp/")
 
-    # Extract the archive to /data/web_static/releases/<archive filename without extension> on the web server
+    # Extract the archive to /data/web_static/releases/<archive
+    # filename without extension> on the web server
     archive_filename = archive_path.split('/')[-1]
     archive_path_no_ext = "/data/web_static/releases/" + archive_filename.split('.')[0]
     run("mkdir -p {}".format(archive_path_no_ext))

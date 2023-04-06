ec04f43bb096e7fbf3f75c23db2eee7d4aba6256
mahlet <akotet2123@gmail.com>
2023-04-06 13:05:16 +0000

    fix

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Fabfile to create a .tgz archive"""
 from datetime import datetime
+import os
 from fabric.api import local
 
 
@@ -8,8 +9,8 @@ def do_pack():
 	folder_name = 'web_static'
 	time = datetime.now().strftime('%Y%m%d%H%M%S')
 	archive_path = 'versions/web_static_{}.tgz'.format(time)
-
-	local('mkdir -p versions')
+	if not os.path.exists('versions'):
+		local('mkdir -p versions')
 	result = local('tar -czvf {} {}'.format(archive_path, folder_name))
 	if result.failed:
 		return None

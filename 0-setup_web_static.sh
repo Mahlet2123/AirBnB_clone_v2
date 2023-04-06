a44f4aa9ee5e39c4aaab98909404059bb581eb7d
mahlet <akotet2123@gmail.com>
2023-04-06 01:03:03 -0700

    Bash script that sets up your web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
-sudo apt-get update
-sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/test/
+if ![ -x "$(command nginx -v)" ];
+then
+        sudo apt-get update
+        sudo apt-get install nginx -y
+fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "<html>
   <head>
   </head>
@@ -11,9 +14,5 @@ echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-if [-L /data/web_static/current];
-then
-        rm  /data/web_static/current
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/

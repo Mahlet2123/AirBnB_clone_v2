fdd142e791e7a1c40444e67d29cf2ed1a1bf1dc7
mahlet <akotet2123@gmail.com>
2023-04-06 14:46:43 -0700

    PEP fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,14 +7,14 @@ from os.path import exists
 from fabric.api import env, put, run
 
 
-env.hosts = ['54.172.89.19', '75.101.217.125']
-env.user = 'ubuntu'
-env.key_filename = '/root/.ssh/id_rsa'
+env.hosts = ["54.172.89.19", "75.101.217.125"]
+env.user = "ubuntu"
+env.key_filename = "/root/.ssh/id_rsa"
 env.use_ssh_config = True
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to your web servers """
+    """distributes an archive to your web servers"""
     if not exists(archive_path):
         return False
 
@@ -25,19 +25,28 @@ def do_deploy(archive_path):
 
     # Extract the archive to /data/web_static/releases/<archive
     # filename without extension> on the web server
-    archive_filename = archive_path.split('/')[-1]
-    archive_path_no_ext = "/data/web_static/releases/" + archive_filename.split('.')[0]
-    filename = archive_filename.split('.')[0]
+    a_filename = archive_path.split("/")[-1]
+    filename = archive_filename.split(".")[0]
+    archive_path_no_ext = "/data/web_static/releases/{}".format(filename)
     run("sudo mkdir -p {}".format(archive_path_no_ext))
-    run("sudo tar -xzf /tmp/{} -C {}/".format(archive_filename, archive_path_no_ext), shell=True)
-    run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
+    run(
+        "sudo tar -xzf /tmp/{} -C {}/".format(a_filename, archive_path_no_ext),
+        shell=True,
+    )
+    run(
+        "sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(
+            filename, filename
+        )
+    )
     run("sudo rm /tmp/{}".format(archive_filename))
 
     # Delete the symbolic link /data/web_static/current from the web server
     run("sudo rm -f /data/web_static/current")
 
-    # Create a new symbolic link /data/web_static/current on the web server, linked to the new version of your code
+    # Create a new symbolic link /data/web_static/current on the web server,
+    # linked to the new version of your code
     run("ln -s {} /data/web_static/current".format(archive_path_no_ext))
 
     print("New version deployed!")
-    return True	
+    return True

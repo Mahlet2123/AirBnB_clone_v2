3a283f529cc767f860b8397607408187aafb6795
mahlet <akotet2123@gmail.com>
2023-04-09 11:43:44 -0700

    puppet file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,42 +1,26 @@
-#sets up your web servers for the deployment of web_static.
-
-exec {'/usr/bin/env apt-get -y update':}
-exec {'/usr/bin/env apt-get -y install nginx':}
-
-package { 'nginx':
-  ensure => installed,
-}
-
-# Create required directories if they don't already exist
-exec { '/usr/bin/env sudo mkdir -p /data/web_static/releases/test/':}
-exec { '/usr/bin/env sudo mkdir -p /data/web_static/shared/':}
-
-# Create index.html file with random content
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html><head></head><body>Holberton School</body></html>',
-  mode    => '0755'
-}
-
-# Create a symbolic link /data/web_static/current linked to the
-# /data/web_static/releases/test/ folder.
-file { '/data/web_static/current':
-  ensure => 'link',
+# puppet manifest preparing a server for static content deployment
+exec {'update package list':
+  command => '/usr/bin/env apt-get -y update',
+} -> 
+exec {'install nginx':
+  command => '/usr/bin/env apt-get -y install nginx',
+} -> 
+exec {'create required directories':
+  command => '/usr/bin/env mkdir -p /data/web_static/releases/test/ && /usr/bin/env mkdir -p /data/web_static/shared/',
+} -> 
+exec {'write "Hello Puppet" to index.html':
+  command => '/usr/bin/env echo "Hello Puppet" | sudo tee /data/web_static/releases/test/index.html',
+} -> 
+file {'/data/web_static/current':
+  ensure => link,
   target => '/data/web_static/releases/test/',
-}
-
-# Give ownership of the /data/ folder to the ubuntu user AND group
-file { '/data/':
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true
-}
-
-# Update the Nginx configuration
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-}->
-file_line { 'Append a line to /etc/nginx/sites-available/default':
-  path => '/etc/nginx/sites-available/default',
-  line => '\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}',
+} -> 
+exec {'add new configuration to NGINX':
+  command => '/usr/bin/env sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+} -> 
+exec {'set ownership of /data/ to ubuntu user':
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
+} -> 
+exec {'restart nginx':
+  command => '/usr/bin/env service nginx restart',
 }

81f150ff7e9c47bf8847b55fe37637c407617db6
mahlet <akotet2123@gmail.com>
2023-04-21 02:15:45 -0700

    PEP fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# script that starts a Flask web application
+""" script that starts a Flask web application """
 from flask import Flask
 
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """ Handles the root url"""
     return "Hello HBNB!"
 
 

764a286d5a83df41f42871f2eeb700a4284a7216
mahlet <akotet2123@gmail.com>
2023-04-21 02:23:41 -0700

    PEP fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,13 +8,15 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """ Handles the root url"""
+    """Handles the root url"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """ Handles the root/hbnb url """
+    """Handles the root/hbnb url"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

b2f9706576b6e768fae690ab7acdc4fda79602c7
Mahlet Seifu <akotet2123@gmail.com>
2023-05-03 23:41:54 +0300

    deploy static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static.
+if ! [ -x "$(command nginx -v)" ];
+then
+        sudo apt-get update
+        sudo apt-get install nginx -y
+fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+test_web="<html><head></head><body>Holberton School</body></html>" 
+echo "$test_web" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart

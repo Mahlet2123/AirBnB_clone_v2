c7326b0274c9e5f5a041c2174516c085b53d2f1a
mahlet <akotet2123@gmail.com>
2023-05-05 04:03:10 +0000

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -8,7 +8,13 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """class Amenity that inherits from BaseModel"""
 
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
 
-    place_amenities = relationship('Place', secondary='place_amenity', viewonly=False, back_populates='amenities')
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenities = relationship(
+            "Place",
+            secondary="place_amenity",
+            viewonly=False,
+            back_populates="amenities",
+        )

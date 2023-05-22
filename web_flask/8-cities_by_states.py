bfa66ee6c03fa19a445350d7203d14595d4529a5
mahlet <akotet2123@gmail.com>
2023-05-21 19:57:51 -0700

    route cities_by_states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
+from flask import Flask
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 
 app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """ for fetching data from the storage engine """
+def city_state_list():
+    """ display a HTML page with the list of all State objects
+    present in DBStorage sorted by name (A->Z) """
     states = list(storage.all(State).values())
-    # states list contains all State objects stored in the database.
     return render_template("8-cities_by_states.html", states=states)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    close the database connection after
-    the request has been processed.
-    """
+def remove_session(exception):
+    """ a Flask teardown function.
+    It is executed after each request to the Flask application. """
     storage.close()
 
 if __name__ == "__main__":
-    storage.reload()
     app.run(host="0.0.0.0", port=5000)
